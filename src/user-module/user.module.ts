72ffd42d146c2fbd6e693d50f7a54806fa6fdaaf
Md Shakil Ahmed <shakilatrai5@gmail.com>
2023-05-31 10:49:51 +0600

    middleware practice

--- src/user-module/user.module.ts
+++ src/user-module/user.module.ts
@@ -1,10 +1,18 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { LoggerMiddleware } from './middleware';
 
 @Module({
   imports: [],
   controllers: [UserController],
   providers: [UserService],
 })
-export class UserModule {}
+export class UserModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({ path: 'users', method: RequestMethod.GET });
+  }
+}
+
